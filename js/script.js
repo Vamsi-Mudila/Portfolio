df9eaf7bf82823b63971f32395493cad77fb4c1f
Vamsi Mudila <vamsi.mudila@gmail.com>
2025-01-27 22:33:53 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -55,13 +55,14 @@ particlesJS("particles-js", {
 
 // Modal Open/Close Functions
 function openModal(modalId) {
-    document.getElementById(modalId).style.display = "flex";
+    document.getElementById(modalId).style.display = "block";
 }
 
 function closeModal(modalId) {
     document.getElementById(modalId).style.display = "none";
 }
 
+// Close Modal on Outside Click
 window.onclick = function (event) {
     const modals = document.querySelectorAll('.modal');
     modals.forEach((modal) => {
@@ -70,3 +71,4 @@ window.onclick = function (event) {
         }
     });
 };
+
